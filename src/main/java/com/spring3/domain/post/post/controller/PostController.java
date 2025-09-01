5712c87421edf244fd6617fff5d46a57b7ffdbde
Yejin <dpwls8972@gmail.com>
2025-09-01 15:02:17 +0900

    post controller

---
+++ src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -0,0 +1,21 @@
+package com.spring3.domain.post.post.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class PostController {
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write() {
+
+        return """
+                <form action="https://localhost:8080/">
+                  <input type="text" name="title">
+                  <br>
+                  <textarea name="content"></textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """;
+    }
+}
