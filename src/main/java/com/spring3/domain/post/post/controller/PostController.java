560dd49f6de7a3363370b58fde44bac7f3233012
Yejin <dpwls8972@gmail.com>
2025-09-02 12:50:22 +0900

    sorted

--- src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -2,10 +2,19 @@ package com.spring3.domain.post.post.controller;
 
 import com.spring3.domain.post.post.entity.Post;
 import com.spring3.domain.post.post.service.PostService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class PostController {
@@ -16,37 +25,93 @@ public class PostController {
         this.postService = postService;
     }
 
-    public String getWriteFormHtml(String errorMessage) {
+    public String getWriteFormHtml(String errorMessage, String title, String content, String errorFieldName) {
 
         return """
                 <div style="color:red">%s</div>
+                
                 <form method="POST" action="/posts/doWrite">
-                  <input type="text" name="title">
+                  <input type="text" name="title" value="%s" autoFocus>
                   <br>
-                  <textarea name="content"></textarea>
+                  <textarea name="content">%s</textarea>
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """.formatted(errorMessage);
+                
+                <script>
+                    const errorFieldName = "%s";
+                    
+                    if(errorFieldName.length > 0) {
+                        const form = document.querySelector("form");
+                        form[errorFieldName].focus();
+                    }
+                </script>
+                """.formatted(errorMessage, title, content, errorFieldName);
     }
+
+    @AllArgsConstructor
+    @Getter
+    public static class PostWriteForm {
+        @NotBlank(message = "1-제목을 입력해주세요.")
+        @Size(min = 2, max = 10, message = "2-제목은 2글자 이상 10글자 이하로 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "3-내용을 입력해주세요.")
+        @Size(min = 2, max = 100, message = "4-내용은 2글자 이상 100글자 이하로 입력해주세요.")
+        private String content;
+    }
+
 
     @GetMapping("/posts/write")
     @ResponseBody
     public String write() {
-        return getWriteFormHtml("");
+        return getWriteFormHtml("", "", "", "");
     }
 
     @PostMapping("/posts/doWrite")
     @ResponseBody
     public String doWrite(
-            String title,
-            String content
+            @Valid PostWriteForm form, BindingResult bindingResult
     ) {
-        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.");
-        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.");
 
-        Post post = postService.write(title, content);
+        if(bindingResult.hasErrors()) {
+
+            String fieldName = "title";
+
+            // 명령형
+//            List<FieldError> fieldErrorsList = bindingResult.getFieldErrors();
+//
+//            StringBuilder sb = new StringBuilder("");
+//
+//            for(FieldError fieldError : fieldErrorsList) {
+//               sb.append(fieldError.getDefaultMessage());
+//                sb.append("<br>");
+//            }
+//
+//            String errorMessages = sb.toString();
+
+
+            // 스트림
+            String errorMessages = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .sorted()
+                    .collect(Collectors.joining("<br>"));
+
+            return getWriteFormHtml(errorMessages, form.title, form.content, fieldName);
+        }
+
+
+//        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.", title, content, "title");
+//        if(title.length() < 2) return getWriteFormHtml("제목은 2글자 이상 적어주세요.", title, content, "title");
+//        if(title.length() > 10) return getWriteFormHtml("제목은 10글자 이상 넘을 수 없습니다.", title, content, "title");
+//        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.", title, content, "content");
+//        if(content.length() < 2) return getWriteFormHtml("내용은 2글자 이상 적어주세요.", title, content, "content");
+//        if(content.length() > 100) return getWriteFormHtml("내용은 100글자 이상 넘을 수 없습니다.", title, content, "content");
+
+        Post post = postService.write(form.title, form.content);
 
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
+
 }
